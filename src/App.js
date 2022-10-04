d51795185891ae37a93e8a2803a260d4f1e6dd92
sabbirzzaman <sztonmoy7@gmail.com>
2022-10-04 23:14:41 +0600

    home page mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CreateEvent from './components/CreateEvent/CreateEvent';
+import Event from './components/Event/Event';
 import Home from './components/Home/Home';
 import NotFounded from './components/NotFounded/NotFounded';
 
@@ -7,8 +9,8 @@ function App() {
   return (
     <Routes>
      <Route path='/' element={<Home />} />
-     <Route path='/create' element={<Home />} />
-     <Route path='/event' element={<Home />} />
+     <Route path='/create' element={<CreateEvent />} />
+     <Route path='/event' element={<Event />} />
      <Route path='*' element={<NotFounded />} />
     </Routes>
   );
